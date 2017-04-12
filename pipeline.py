b3ae791bc0593d1b2ef0563caa117d04828907d3
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-04-12 14:37:50 +0200

    With TensorBoard Visualization

--- pipeline.py
+++ pipeline.py
@@ -22,7 +22,17 @@ random_seed = int(time.time() % 10000 )
 random.seed(random_seed)  # for reproducibility
 print('   random seed =', random_seed)
 
-
+def variable_summaries(var):
+  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
 
 # tool functions
 print('   python function setup')
@@ -54,9 +64,12 @@ def avg_pool_10x10(x):
                            strides=[1, 10, 10, 1], padding='SAME')
 
 def histogram(x, nbins):
-  return(tf.histogram_fixed_width(x, 
-                                  value_range = [-1.0,1.0], 
-                                  nbins = nbins, dtype = tf.float32))
+  h = tf.histogram_fixed_width(x, 
+                               value_range = [0.0,255.0], 
+                               nbins = nbins, dtype = tf.float32)
+  # mom = tf.nn.moments(h, axes = [0]) 
+  # h = (h-mom[0])/mom[1]
+  return(h)
 
   
   # start process
@@ -75,13 +88,16 @@ data = il.Database_loader('/media/nicolas/Home/nicolas/Documents/Stage 3A/Test',
 print('   create model ...')
 # input layer. One entry is a float size x size, 3-channels image. 
 # None means that the number of such vector can be of any lenght.
-x = tf.placeholder(tf.float32, [None, image_size, image_size, 1])
+with tf.name_scope('Input_Data'):
+  x = tf.placeholder(tf.float32, [None, image_size, image_size, 1])
 
 # reshape the input data:
 # size,size: width and height
 # 1: color channels
 # -1 :  ???
-x_image = tf.reshape(x, [-1,image_size, image_size, 1])
+  x_image = tf.reshape(x, [-1,image_size, image_size, 1])
+  with tf.name_scope('Image_Visualization'):
+    tf.summary.image('Input_Data', x_image)
 
 # Filtering with laplacian filter
 # laplacian = tf.constant([[0, -1, 0], [-1, 4, -1], [0, -1, 0]], tf.float32)
@@ -115,29 +131,51 @@ x_image = tf.reshape(x, [-1,image_size, image_size, 1])
 # conv_matrix nb channel ??? : 1
 # nb matrices : 32 ?
 
-batch_size = tf.shape(x)[0] 
 
-W_conv1 = weight_variable([5, 5, 1, 32], seed = random_seed)
-b_conv1 = bias_variable([32])
+with tf.name_scope('Conv1'):
 
-# relu on the conv layer
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+  with tf.name_scope('Weights'):
+    W_conv1 = weight_variable([5, 5, 1, 32], seed = random_seed)
+    variable_summaries(W_conv1)
+  with tf.name_scope('Bias'):
+    b_conv1 = bias_variable([32])
+    variable_summaries(b_conv1)
 
-print(h_conv1.shape)
-#m_pool = max_pool_2x2(h_conv1)
-#
+  # relu on the conv layer
+  h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1, name = 'activated')
+  tf.summary.histogram('activated', h_conv1)
+
+  with tf.variable_scope('Conv1_visualization'):
+    tf.summary.image('conv1/filters', W_conv1[:,:,:,0:1])
+
+
+# with tf.name_scope('MaxPool'):
+#   m_pool = max_pool_2x2(h_conv1)
+
 # second conv 
-# W_conv2 = weight_variable([5, 5, 32, 64])
-# b_conv2 = bias_variable([64])
-# h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+with tf.name_scope('Conv2'):
+  with tf.name_scope('Weights'):
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    variable_summaries(W_conv1)
+  with tf.name_scope('Bias'):
+    b_conv2 = bias_variable([64])
+    variable_summaries(b_conv2)
 
-nbins = 50
+  h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+  tf.summary.histogram('activated', h_conv2)
 
-function_to_map = lambda x: tf.stack([tf.nn.l2_normalize(histogram(x[:,:,i], nbins), dim = 0) for i in range(32)])
+  with tf.variable_scope('Conv2_visualization'):
+    tf.summary.image('conv2/filters', W_conv2[:,:,:,0:1])
 
-hist = tf.map_fn(function_to_map, h_conv1)
+with tf.name_scope('MaxPool'):
+  m_pool = max_pool_2x2(h_conv2)
 
-print(hist.shape)
+# nbins = 100
+
+# with tf.name_scope('Histograms'):
+#   function_to_map = lambda x: tf.stack([histogram(x[:,:,i], nbins) for i in range(32)])
+#   hist = tf.map_fn(function_to_map, h_conv1)
+#   variable_summaries(hist)
 
 # max pool
 #m_pool3 = max_pool_10x10(h_conv1)
@@ -154,34 +192,65 @@ print(hist.shape)
 # flattern 
 # with the 2 pooling, the image size is 7x7
 # there are 64 conv matrices
-h_pool2_flat = tf.reshape(hist, [-1, nbins*32])
+size = tf.cast((image_size/2)*(image_size/2)*64, tf.int32)
+h_pool1_flat = tf.reshape(m_pool, [-1, size], name = "Flatten_Conv")
+
+# h_pool2_flat = tf.reshape(hist, [-1, nbins*32], name = "Flatten_Hist")
+
+
+# concat = tf.concat([h_pool1_flat, h_pool2_flat], 1, name = "Concat")
 
 # Densely Connected Layer
 # we add a fully-connected layer with 1024 neurons 
-W_fc1 = weight_variable([nbins*32, 1024])
-b_fc1 = bias_variable([1024])
-# put a relu
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# size_dense = tf.cast((image_size/2)*(image_size/2)*32 + nbins*32, tf.int32)
+
+with tf.variable_scope('Dense1'):
+  with tf.name_scope('Weights'):
+    W_fc1 = weight_variable([size, 1024])
+    variable_summaries(W_fc1)
+  with tf.name_scope('Bias'):
+    b_fc1 = bias_variable([1024])
+    variable_summaries(b_fc1)
+  # put a relu
+  h_fc1 = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc1) + b_fc1, name = 'activated')
+  tf.summary.histogram('activated', h_fc1)
 
 # dropout
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+with tf.name_scope('Dropout1'):
+  keep_prob = tf.placeholder(tf.float32)
+  tf.summary.scalar('dropout_keep_probability', keep_prob)
+  h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # Densely Connected Layer
 # we add a fully-connected layer with 1024 neurons 
-W_fc2 = weight_variable([1024, 1024])
-b_fc2 = bias_variable([1024])
-# put a relu
-h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+with tf.variable_scope('Dense2'):
+  with tf.name_scope('Weights'):
+    W_fc2 = weight_variable([1024, 1024]) 
+    variable_summaries(W_fc2)
+  with tf.name_scope('Bias'):
+    b_fc2 = bias_variable([1024])
+    variable_summaries(b_fc2)
+  # put a relu
+  h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name = 'activated')
+  tf.summary.histogram('activated', h_fc2)
 
 # dropout
-keep_prob2 = tf.placeholder(tf.float32)
-h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
+with tf.name_scope('Dropout2'):
+  keep_prob2 = tf.placeholder(tf.float32)
+  tf.summary.scalar('dropout_keep_probability', keep_prob)
+  h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
 
 # readout layer
-W_fc2 = weight_variable([1024, data.nb_class])
-b_fc2 = bias_variable([data.nb_class])
-y_conv = tf.matmul(h_fc2_drop, W_fc2) + b_fc2
+with tf.variable_scope('Readout'):
+  with tf.name_scope('Weights'):
+    W_fc3 = weight_variable([1024, data.nb_class])
+    variable_summaries(W_fc3)
+  with tf.name_scope('Bias'):
+    b_fc3 = bias_variable([data.nb_class])
+    variable_summaries(b_fc3)
+  y_conv = tf.matmul(h_fc2_drop, W_fc3) + b_fc3
 
 # support for the learning label
 y_ = tf.placeholder(tf.float32, [None, data.nb_class])
@@ -193,18 +262,28 @@ y_ = tf.placeholder(tf.float32, [None, data.nb_class])
 print('   setup loss function and optimizer ...')
 
 # softmax to have normalized class probabilities + cross-entropy
-softmax_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels = y_, logits = y_conv)
-#print('\nsoftmax_cross_entropy shape : ', softmax_cross_entropy.get_shape() )
+with tf.name_scope('cross_entropy'):
 
-cross_entropy_mean = tf.reduce_mean(softmax_cross_entropy)
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_mean)
+  softmax_cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels = y_, logits = y_conv)
+  #print('\nsoftmax_cross_entropy shape : ', softmax_cross_entropy.get_shape() )
+  with tf.name_scope('total'):
+    cross_entropy_mean = tf.reduce_mean(softmax_cross_entropy)
+
+tf.summary.scalar('cross_entropy', cross_entropy_mean)
+
+with tf.name_scope('train'):
+  train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_mean)
 
 print('   test ...')
 # 'correct_prediction' is a function. argmax(y, 1), here 1 is for the axis number 1
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 
 # 'accuracy' is a function: cast the boolean prediction to float and average them
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+with tf.name_scope('accuracy'):
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+tf.summary.scalar('accuracy', accuracy)
+
 
 
 
@@ -213,8 +292,13 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print('   start session ...')
 sess = tf.InteractiveSession()
 
+merged = tf.summary.merge_all()
+train_writer = tf.summary.FileWriter('/home/nicolas/Documents/summaries',
+                                      sess.graph)
+
 print('   variable initialization ...')
 tf.global_variables_initializer().run()
+
 
 
 # Train
@@ -243,8 +327,9 @@ for i in range(81): # in the test 20000
     batch_size = 50
     batch = data.get_next_train_batch(batch_size, True, True)
 #    print('train ...')
-    feed_dict = {x: batch[0], y_: batch[1], keep_prob: 0.5}
-    train_step.run(feed_dict)
+    feed_dict = {x: batch[0], y_: batch[1], keep_prob: 0.7}
+    summary, _ = sess.run([merged, train_step], feed_dict = feed_dict)
+    train_writer.add_summary(summary, i)
 
     
 # history
