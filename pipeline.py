cd0086c1b5f77f425728336743a3a4081cf44d53
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-05-01 20:17:34 +0900

    Last parameters

--- pipeline.py
+++ pipeline.py
@@ -610,7 +610,7 @@ if __name__ == '__main__':
     database_path = '/home/nicolas/Database/level-design_raise_100/'
 
   image_size = 100
-  nb_train_batch = 1000
+  nb_train_batch = 12000
   nb_test_batch = 80
   nb_validation_batch = 40
 
