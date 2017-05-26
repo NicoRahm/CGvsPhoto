edfd81adb4636c359e14bdc4c22ecb17cc397030
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-05-26 16:24:18 +0900

    Last parameters

--- pipeline.py
+++ pipeline.py
@@ -1179,12 +1179,12 @@ if __name__ == '__main__':
   # clf.lda_training(nb_train_batch = 800, nb_test_batch = 80)
 
   if config == 'server':
-    test_data_path = '/work/smg/v-nicolas/Fun/'
+    test_data_path = '/work/smg/v-nicolas/level-design_raise/test/'
   else: 
     test_data_path = '/home/nicolas/Database/level-design_raise/test/'
 
   clf.test_total_images(test_data_path = test_data_path,
-                        nb_images = 86, decision_rule = 'weighted_vote',
+                        nb_images = 720, decision_rule = 'weighted_vote',
                         show_images = False, 
                         save_images = False,
                         only_green = True)
