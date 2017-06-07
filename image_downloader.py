7b096ae3d171006342f422888369f47be77903e5
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-06-07 19:06:43 +0900

    Removing useless files

--- image_downloader.py
+++
@@ -1,7 +0,0 @@
-id_deb = 58406
-id_fin = 58743
-with open("/home/nicolas/Documents/data_level_design/download", 'w') as file:
-	for i in range(id_deb, id_fin + 1):
-		file.write(" wget -O level_design_" + str(i) + 
-			".jpg 'http://www.level-design.org/referencedb/action.php?id=" 
-			+ str(i) + "&part=e&download' \n")
