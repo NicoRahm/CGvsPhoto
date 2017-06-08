f4612f7df737b7fc02582a20fa087da594c1df8c
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-06-08 11:06:21 +0900

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='CGvsPhoto',
+      version='0.1',
+      description='A deep-learning method for distinguishing computer graphics from real photogrphic images',
+      url='https://github.com/NicoRahm/CGvsPhoto',
+      author='Nicolas Rahmouni',
+      author_email='nicolas.rahmouni@polytechnique.edu',
+      packages=['CGvsPhoto'],
+      zip_safe=False)
