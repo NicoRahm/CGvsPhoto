622b7306d70ce6d67caf8e0593fc7e423f5921a4
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-06-09 11:39:20 +0900

    Adding new examples

---
+++ examples/test_splicing.py
@@ -0,0 +1,21 @@
+from CGvsPhoto import Model
+# to change to your splicing database
+splicing_data_path = '/home/nicolas/Database/splicing3/'
+
+# to change to your training database
+database_path = '/home/nicolas/Database/level-design_raise/'
+
+# to change to the format of your image
+image_size = 100
+
+# define a single-image classifier
+clf = Model(database_path, image_size, config = 'Personal', filters = [32,64],
+            batch_size = 50, feature_extractor = 'Stats')
+
+
+# tests classifier on splicing images
+clf.test_splicing(data_path = splicing_data_path, 
+                  nb_images = 50,
+                  minibatch_size = 25,
+                  show_images = False,
+                  save_images = True)
