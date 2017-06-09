2599ee65deb9e6b2aa5e54c688fdcb2ce2bb3c32
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-06-09 16:30:37 +0900

    First release on PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 from setuptools import find_packages
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='CGvsPhoto',
-      version='0.1',
+      version='0.2.0',
       description='A deep-learning method for distinguishing computer graphics from real photogrphic images',
+      long_description=readme(),
       url='https://github.com/NicoRahm/CGvsPhoto',
       author='Nicolas Rahmouni',
       author_email='nicolas.rahmouni@polytechnique.edu',
-      packages=find_packages(exclude = ['examples']),
+      packages=find_packages(exclude = ['examples', 'weights']),
       license='MIT',
+      include_package_data=True,
       install_requires=['tensorflow>=1.0.1','numpy>=1.6.1',
       					'scikit-learn>=0.18.1', 'Pillow>=3.1.2', 
       					'matplotlib>=1.3.1'],
