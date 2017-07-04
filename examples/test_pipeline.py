4061bec2a81dc950b420d4ef666300a9f0c0eaa2
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-04 18:01:25 +0900

    Adding constraints on first layer

--- examples/test_pipeline.py
+++ examples/test_pipeline.py
@@ -7,7 +7,7 @@ image_size = 100
 
 # define a single-image classifier
 clf = Model(database_path, image_size, config = 'Server', filters = [32,64],
-            batch_size = 50, feature_extractor = 'Stats', remove_context = False)
+            batch_size = 50, feature_extractor = 'Stats', remove_context = True)
 
 
 # trains the classifier and test it on the testing set
