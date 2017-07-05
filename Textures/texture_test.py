412f1021f0a5f785a4c7ffbcb1f1e1bacc0f0d57
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-05 15:51:47 +0900

    Last parameters

--- Textures/texture_test.py
+++ Textures/texture_test.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-from sklearn.neural_network import MLPClassifier
+# from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
@@ -174,8 +174,8 @@ if __name__ == '__main__':
 	# pca = PCA(n_components=n_comp)
 
 	clf_svm = LinearSVC()
-	clf_mlp = MLPClassifier(hidden_layer_sizes = (1024,128), activation='relu',
-							solver = 'adam')
+	# clf_mlp = MLPClassifier(hidden_layer_sizes = (1024,128), activation='relu',
+	# 						solver = 'adam')
 
 
 
@@ -244,7 +244,7 @@ if __name__ == '__main__':
 	del(features)
 
 	print('Fitting Gaussian Mixture Model...')
-	K = 64
+	K = 128
 	gmm = GaussianMixture(n_components=K, covariance_type='diag')
 	gmm.fit(np.reshape(features_PCA, [features_PCA.shape[0]*nb_mini_patch, n_comp]))
 
@@ -270,8 +270,8 @@ if __name__ == '__main__':
 	clf_svm.fit(fisher_train, y_train)
 	# clf.fit(np.reshape(features_PCA, [nb_train_batch*batch_size, n_comp*nb_mini_patch]), y_train)
 
-	print('Fitting MLP...')
-	clf_mlp.fit(fisher_train, y_train)
+	# print('Fitting MLP...')
+	# clf_mlp.fit(fisher_train, y_train)
 
 
 	del(fisher_train, y_train)
@@ -342,11 +342,12 @@ if __name__ == '__main__':
 	print('Prediction...')
 	y_pred_svm = clf_svm.predict(fisher_test)
 	# y_pred = clf.predict(np.reshape(features_test_PCA, [nb_test_batch*batch_size, n_comp*nb_mini_patch]))
-	y_pred_mlp = clf_mlp.predict(fisher_test)
+	
+	# y_pred_mlp = clf_mlp.predict(fisher_test)
 
 	print('Computing score...')
 	score_svm = accuracy_score(y_pred_svm, y_test)
-	score_mlp = accuracy_score(y_pred_mlp, y_test)
+	# score_mlp = accuracy_score(y_pred_mlp, y_test)
 
 	print('Accuracy SVM : ' + str(score_svm))
-	print('Accuracy MLP : ' + str(score_mlp))
+	# print('Accuracy MLP : ' + str(score_mlp))
