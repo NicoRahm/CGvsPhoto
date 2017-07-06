4cd3271306b68a4fbe944ddbd60cd2f4937da439
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-06 17:34:54 +0900

    Adding calibration

--- Textures/texture.py
+++ Textures/texture.py
@@ -15,6 +15,7 @@ from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
+from svm.calibration import CalibratedClassifierCV
 
 from multiprocessing import Pool
 
@@ -121,7 +122,7 @@ class Texture_model:
 
 		self.gmm = GaussianMixture(n_components=K_gmm, 
 								   covariance_type='diag')
-		self.clf_svm = LinearSVC()
+		self.clf_svm = CalibratedClassifierCV(LinearSVC())
 
 
 
