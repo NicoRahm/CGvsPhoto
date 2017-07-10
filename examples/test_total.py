dc60b5472fc2ad6e9ea5634e4a78aec93875e68d
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-10 14:11:30 +0900

    Last parameters

--- examples/test_total.py
+++ examples/test_total.py
@@ -1,4 +1,5 @@
 from CGvsPhoto import Model
+import os 
 # to change to your favorite database
 database_path = '/work/smg/v-nicolas/level-design_raise_100_color/'
 test_data_path = '/work/smg/v-nicolas/face_DB/'
@@ -11,9 +12,11 @@ clf = Model(database_path, image_size, config = 'Server', filters = [32,32,64],
             batch_size = 50, feature_extractor = 'Stats', remove_context = True)
 
 
+nb_images = len([name for name in os.listdir(test_data_path) if os.path.isfile(name)])
 # test classifier on total image
 clf.test_total_images(test_data_path = test_data_path,
-                      nb_images = 216, decision_rule = 'weighted_vote',
+                      nb_images = nb_images, 
+                      decision_rule = 'weighted_vote',
                       show_images = False, 
                       save_images = False,
                       only_green = True,
