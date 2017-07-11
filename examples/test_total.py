2f2e579f8b27fb7183127688eb405f8b0fb62bfb
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-11 11:06:26 +0900

    Last parameters

--- examples/test_total.py
+++ examples/test_total.py
@@ -2,7 +2,7 @@ from CGvsPhoto import Model
 import os 
 # to change to your favorite database
 database_path = '/work/smg/v-nicolas/level-design_raise_100_color/'
-test_data_path = '/work/smg/v-nicolas/face_DB/'
+test_data_path = '/work/smg/v-nicolas/level-design_raise/'
 
 # to change to the format of your image
 image_size = 100
@@ -15,7 +15,7 @@ clf = Model(database_path, image_size, config = 'Server', filters = [32,32,64],
 nb_images = len([name for name in os.listdir(test_data_path + '/CGG') if os.path.isfile(name)]) + len([name for name in os.listdir(test_data_path + '/Real') if os.path.isfile(name)])
 # test classifier on total image
 clf.test_total_images(test_data_path = test_data_path,
-                      nb_images = nb_images, 
+                      nb_images = 720, 
                       decision_rule = 'weighted_vote',
                       show_images = False, 
                       save_images = False,
