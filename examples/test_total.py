790c325d4ffe30d94370ade157475b5c471f176d
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-11 11:09:27 +0900

    Last parameters

--- examples/test_total.py
+++ examples/test_total.py
@@ -2,7 +2,7 @@ from CGvsPhoto import Model
 import os 
 # to change to your favorite database
 database_path = '/work/smg/v-nicolas/level-design_raise_100_color/'
-test_data_path = '/work/smg/v-nicolas/level-design_raise/'
+test_data_path = '/work/smg/v-nicolas/level-design_raise/test/'
 
 # to change to the format of your image
 image_size = 100
@@ -12,7 +12,7 @@ clf = Model(database_path, image_size, config = 'Server', filters = [32,32,64],
             batch_size = 50, feature_extractor = 'Stats', remove_context = True)
 
 
-nb_images = len([name for name in os.listdir(test_data_path + '/CGG') if os.path.isfile(name)]) + len([name for name in os.listdir(test_data_path + '/Real') if os.path.isfile(name)])
+# nb_images = len([name for name in os.listdir(test_data_path + '/CGG') if os.path.isfile(name)]) + len([name for name in os.listdir(test_data_path + '/Real') if os.path.isfile(name)])
 # test classifier on total image
 clf.test_total_images(test_data_path = test_data_path,
                       nb_images = 720, 
