c6f99ce4d2982ef1d7b2e29124216304a768ad47
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-11 12:24:09 +0900

    With saving data

--- Textures/texture.py
+++ Textures/texture.py
@@ -267,7 +267,7 @@ class Texture_model:
 			if save_fisher: 
 
 				dump_name = input('Name of the dump file : ')
-				pickle.dump([fisher_train, y_train], self.dump_data_directory + '/' + dump_name)
+				pickle.dump([fisher_train, y_train], self.dump_data_directory + '/' + dump_name + '.pkl')
 
 		# Plotting boxplot
 
@@ -283,7 +283,7 @@ class Texture_model:
 		# 	plt.show()
 
 		else: 
-			data = pickle.load(self.dump_data_directory + '/' + fisher_data_name)
+			data = pickle.load(self.dump_data_directory + '/' + fisher_data_name + '.pkl')
 			fisher_train = data[0]
 			y_train = data[1]
 
