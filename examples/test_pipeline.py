2907c3afc0cfa970aa3d3c4441b913438a67dc76
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-14 17:58:44 +0900

    Last parameters

--- examples/test_pipeline.py
+++ examples/test_pipeline.py
@@ -12,7 +12,7 @@ clf = Model(database_path, image_size, config = 'Server', filters = [32,32,64],
 
 
 # trains the classifier and test it on the testing set
-clf.train(nb_train_batch = 9000,
+clf.train(nb_train_batch = 10000,
           nb_test_batch = 80, 
           nb_validation_batch = 40,
           show_filters = False)
