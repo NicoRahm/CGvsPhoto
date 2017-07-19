8886b19c8405de465e6b73bb7c42a10f78af0220
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-19 15:47:01 +0900

    Last parameters

--- examples/test_pipeline.py
+++ examples/test_pipeline.py
@@ -1,18 +1,20 @@
 from CGvsPhoto import Model
 # to change to your favorite database
-database_path = '/work/smg/v-nicolas/level-design_raise_100_color/'
+# database_path = '/work/smg/v-nicolas/level-design_raise_100_color/'
+database_path = '/work/smg/v-nicolas/face_DB_100_2/'
+
 
 # to change to the format of your image
 image_size = 100
 
 # define a single-image classifier
-clf = Model(database_path, image_size, config = 'Server', filters = [32,64],
-            batch_size = 50, feature_extractor = 'Stats', remove_context = False, 
-            remove_filter_size = 7)
+clf = Model(database_path, image_size, config = 'Server', filters = [32,32,64],
+            batch_size = 50, feature_extractor = 'Stats', remove_context = True, 
+            remove_filter_size = 5)
 
 
 # trains the classifier and test it on the testing set
-clf.train(nb_train_batch = 9000,
+clf.train(nb_train_batch = 5000,
           nb_test_batch = 80, 
           nb_validation_batch = 40,
           validation_frequency = 20,
