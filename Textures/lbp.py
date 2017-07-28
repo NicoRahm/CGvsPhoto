462da4a076e3850d16f6cc473638903ac5855852
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-07-28 11:39:19 +0900

    Last parameters

--- Textures/lbp.py
+++ Textures/lbp.py
@@ -263,15 +263,16 @@ def test_total_images(test_data_path, nb_images, classifier,
 		nb_im = 0
 		while j < batch_size:
 
+			d = []
 			for k in range(j, j+minibatch_size): 
-				data.append([batch[k], label[k]])
+				d.append([batch[k], label[k]])
 
 			to_compute = [i for i in range(minibatch_size)]
 			result = pool.starmap(partial(compute_features, 
 										batch_size = 1, 
 										nb_batch = minibatch_size, 
 										mode = 'lbp'),
-										zip(data, to_compute)) 
+										zip(d, to_compute)) 
 
 			pred = classifier.predict_proba(result[:,0])
 					
