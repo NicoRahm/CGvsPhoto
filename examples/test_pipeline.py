0e054d33560f60004b1de8ffe78cdfe148681b0f
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-08-03 13:40:58 +0900

    Last parameters

--- examples/test_pipeline.py
+++ examples/test_pipeline.py
@@ -9,12 +9,12 @@ image_size = 100
 
 # define a single-image classifier
 clf = Model(database_path, image_size, config = 'Server', filters = [32,32,64],
-            batch_size = 50, feature_extractor = 'Stats', remove_context = True, 
+            batch_size = 50, feature_extractor = 'Stats', remove_context = False, 
             remove_filter_size = 5, only_green = False)
 
 
 # trains the classifier and test it on the testing set
-clf.train(nb_train_batch = 3000,
+clf.train(nb_train_batch = 9000,
           nb_test_batch = 80, 
           nb_validation_batch = 40,
           validation_frequency = 20,
