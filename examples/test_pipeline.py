44d5aa39c01e28a0b6e0609798ea5cbabe6e9915
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-08-07 11:00:49 +0900

    Last parameters

--- examples/test_pipeline.py
+++ examples/test_pipeline.py
@@ -8,13 +8,13 @@ database_path = '/work/smg/v-nicolas/level-design_raise_100_color/'
 image_size = 100
 
 # define a single-image classifier
-clf = Model(database_path, image_size, config = 'Server', filters = [32,32,64,64],
+clf = Model(database_path, image_size, config = 'Server', filters = [32,64,64],
             batch_size = 50, feature_extractor = 'Stats', remove_context = False, 
             remove_filter_size = 5, only_green = False)
 
 
 # trains the classifier and test it on the testing set
-clf.train(nb_train_batch = 10000,
+clf.train(nb_train_batch = 0,
           nb_test_batch = 80, 
           nb_validation_batch = 40,
           validation_frequency = 20,
