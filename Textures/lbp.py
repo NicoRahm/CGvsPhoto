88f9a03b2c35675d1a223dc3fcbb475e110de428
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2017-08-09 17:45:44 +0900

    Last parameters

--- Textures/lbp.py
+++ Textures/lbp.py
@@ -428,10 +428,10 @@ if __name__ == '__main__':
 
 	# clf = SVC()
 
-	# clf = CalibratedClassifierCV(LinearSVC())
+	clf = CalibratedClassifierCV(LinearSVC())
 
-	clf = xgb.XGBClassifier(max_depth = 3, learning_rate = 0.1, 
-							n_estimators = 150)
+	# clf = xgb.XGBClassifier(max_depth = 3, learning_rate = 0.1, 
+	# 						n_estimators = 150)
 
 
 	print('Fitting Classifier...')
