143ee4ba9f7cc7ffd6e16c98302dd534051e8a6e
matsuokayuto <52948621+matsuokayuto@users.noreply.github.com>
2019-09-07 18:14:01 +0900

    Change address

--- examples/create_DB.py
+++ examples/create_DB.py
@@ -5,7 +5,7 @@ source_real = "/home/secure/CGvsPhoto/Database/PG/"
 # Change to the source of CG images
 source_CG = "/home/secure/CGvsPhoto/Database/CG/"
 # The directory where the database will be saved
-target_dir = '/home/secure/CGvsPhoto/Database/test/'
+target_dir = '/home/secure/CGvsPhoto/Database/test_DB/'
 
 # Construct a database with an equilibrated CG/Real ratio
 # Formatted to be used with the image_loader
